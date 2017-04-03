08eef7c06dee04c5f75c1ae0838b892a845945fb
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2017-04-03 11:57:39 +0300

    add slf4j logger support
      $ mvn clean compile assembly:single
      $ java -jar target/my-app-1.0-SNAPSHOT-jar-with-dependencies.jar
      [main] INFO com.mycompany.app.App - Hello World
      [main] WARN com.mycompany.app.App - Hello World
      [main] ERROR com.mycompany.app.App - Hello World
      Hello World!
    
    Signed-off-by: Hiroshi Doyu <hiroshi.doyu@ericsson.com>

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,8 @@
 package com.mycompany.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
+	Logger LOG = LoggerFactory.getLogger(App.class);
+	LOG.trace("Hello World");
+	LOG.debug("Hello World");
+	LOG.info("Hello World");
+	LOG.warn("Hello World");
+	LOG.error("Hello World");
         System.out.println( "Hello World!" );
     }
 }
